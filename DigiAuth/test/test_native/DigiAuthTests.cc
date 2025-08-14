ad5318ae763e88cd41df189e84cfe54b2af8e83c
anotherSimpleCoder <amsakan@proton.me>
2025-08-14 17:36:54 +0200

    Adapted DigiAuth code to spec changes (removed device ID)

--- DigiAuth/test/test_native/DigiAuthTests.cc
+++ DigiAuth/test/test_native/DigiAuthTests.cc
@@ -5,22 +5,22 @@ using DigiAuth::DigiAuthMessage;
 using DigiAuth::Status;
 
 TEST(DigiAuthTests, EncodeRightMessage_ShouldBeEqual) {
-        auto testMessage = DigiAuthMessage{1,Status::SUCCESS};
-        auto referenceCode = 0b10100101;
+        auto testMessage = DigiAuthMessage{Status::SUCCESS};
+        auto referenceCode = 0b10100001;
 
         ASSERT_EQ(DigiAuth::encode(testMessage), referenceCode);
 }
 
 TEST(DigiAuthTests, EncodeWrongMessage_ShouldBeInequal) {
-        auto testMessage = DigiAuthMessage{1,Status::START};
-        auto referenceCode = 0b10100101;
+        auto testMessage = DigiAuthMessage{Status::FAIL};
+        auto referenceCode = 0b10100001;
 
         ASSERT_NE(DigiAuth::encode(testMessage), referenceCode);
 }
 
 TEST(DigiAuthTests, DecodeRightMessage_ShouldBeEqual) {
-        auto testCode = 0b10101110;
-        auto referenceMessage = DigiAuthMessage{3, Status::FAIL};
+        auto testCode = 0b10100010;
+        auto referenceMessage = DigiAuthMessage{Status::FAIL};
 
         ASSERT_EQ(DigiAuth::decode(testCode), referenceMessage);
 }
@@ -28,9 +28,23 @@ TEST(DigiAuthTests, DecodeRightMessage_ShouldBeEqual) {
 
 TEST(DigiAuthtests, DecodeWrongMessage_ShouldBeInequal) {
         auto testCode = 0b10101111;
-        auto referenceMessage = DigiAuthMessage{3, Status::FAIL};
+        auto referenceMessage = DigiAuthMessage{Status::FAIL};
 
         ASSERT_NE(DigiAuth::decode(testCode), referenceMessage);
+}
+
+TEST(DigiAuthtests, DecodeInvalidCode_ShouldBeInequal) {
+        auto testCode = 255;
+        auto referenceMessage = DigiAuthMessage{Status::SUCCESS};
+
+        ASSERT_NE(DigiAuth::decode(testCode), referenceMessage);
+}
+
+TEST(DigiAuthtests, DecodeInvalidCode_ShouldBeInvalid) {
+        auto testCode = 255;
+        auto referenceMessage = DigiAuthMessage{Status::INVALID};
+
+        ASSERT_EQ(DigiAuth::decode(testCode), referenceMessage);
 }
 
 int main(int argc, char **argv) {
